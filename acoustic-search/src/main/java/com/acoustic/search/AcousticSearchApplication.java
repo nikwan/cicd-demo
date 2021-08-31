1ffe14bd0f40049be104906383bb7343a5b5582c
nikwan <nikhil.courser@gmail.com>
2021-08-31 15:47:58 +0530

    zipkin distributed integration

--- acoustic-search/src/main/java/com/acoustic/search/AcousticSearchApplication.java
+++ acoustic-search/src/main/java/com/acoustic/search/AcousticSearchApplication.java
@@ -1,5 +1,9 @@
 package com.acoustic.search;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,9 +19,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.acoustic.search.entities"})
 @ComponentScan(basePackages = { "com.acoustic.search", "com.acoustic.search.controller"})
 public class AcousticSearchApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(AcousticSearchApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcousticSearchApplication.class, args);
+	}
+	
+	@PostConstruct
+	void init() {
+		log.info("****************************************");
+		log.debug("version:{}", "V10.0");
+		log.info("****************************************");
 	}
 
 }
