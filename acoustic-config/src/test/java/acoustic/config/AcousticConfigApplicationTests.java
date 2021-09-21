42fadee38ee8d2020d4deebb0d241a0817d52b47
nikwan <nikhil.courser@gmail.com>
2021-09-21 13:39:31 +0530

    acoustic-config initial commit

---
+++ acoustic-config/src/test/java/acoustic/config/AcousticConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package acoustic.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcousticConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
